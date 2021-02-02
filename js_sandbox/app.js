213343d1e7ced98e1a34600a166433142ea85763
Josh Nelson <jshnelson4@gmail.com>
2021-02-02 18:20:18 -0500

    object oriented practice

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,55 +1,29 @@
-// string
+class Person {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 
-const name1 = 'Josh';
-const name2 = new String('Josh');
-
-name2.foot = 'right';
-console.log(name2);
-
-console.log(typeof name2);
-
-
-if (name1 === 'Josh') {
-    console.log('YES');
-} else {
-    console.log('NO');
+    greeting() {
+        return `Hello there ${this.firstName} ${this.lastName}`;
+    }
 }
 
-// number
+class Customer extends Person {
+    constructor(firstName, lastName, phone, membership) {
+        super(firstName, lastName);
 
-const num1 = 5;
-const num2 = new Number(10);
+        this.phone = phone;
+        this.membership = membership;
+    }
 
-// bool
-
-const bool1 = true;
-const bool2 = new Boolean(true);
-
-// func
-
-const getSum1 = function(x, y) {
-    return x + y;
+    static getMembershipCost() {
+        return 500;
+    }
 }
 
-const getSum2 = new Function('x', 'y', 'return 1 + 43');
-console.log(getSum1(1,43));
-console.log(getSum2);
+const josh = new Customer('josh', 'nelson', '111-111-1111', 'deluxe premium deluxe');
 
-// obj
-
-const josh1 = { name: 'Josh' };
-const josh2 = new Object({ name: 'Josh' });
-console.log(josh2);
-
-// arrays
-
-const arr1 = [1,2,3,4];
-const arr2 = new Array(1,2,3,4);
-console.log(arr2);
-
-// regular expressions
-
-const reg1 = /\w+/;
-const reg2 = new RegExp('\\w+');
-console.log(reg1);
-console.log(reg2);
+console.log(josh);
+console.log(josh.greeting());
+console.log(Customer.getMembershipCost());
