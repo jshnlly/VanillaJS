a35e7cc56fcf1748ff289bdbcd8e0a739fb9a2a4
Josh Nelson <jshnelson4@gmail.com>
2021-02-02 08:48:23 -0500

    constructors practice

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,34 +1,55 @@
-// set local storage item
+// string
 
-localStorage.setItem('name', 'josh');
+const name1 = 'Josh';
+const name2 = new String('Josh');
 
-// session storage
+name2.foot = 'right';
+console.log(name2);
 
-sessionStorage.setItem('name', 'faceless man');
-
-const name = localStorage.getItem('name');
-console.log(name);
-
-document.querySelector('form').addEventListener('submit', function(e) {
-    const task = document.getElementById('task').value;
-
-    let tasks;
-
-    if(localStorage.getItem('tasks') === null) {
-        tasks = [];
-    } else {
-        tasks = JSON.parse(localStorage.getItem('tasks'));
-    }
-
-    tasks.push(task);
+console.log(typeof name2);
 
 
+if (name1 === 'Josh') {
+    console.log('YES');
+} else {
+    console.log('NO');
+}
 
-    localStorage.setItem('task', JSON.stringify(task));
-    alert('task saved');
-});
+// number
 
-const tasks = JSON.parse(localStorage.getItem('tasks'));
-tasks.forEach(function(task) {
-    console.log(task);
-});
+const num1 = 5;
+const num2 = new Number(10);
+
+// bool
+
+const bool1 = true;
+const bool2 = new Boolean(true);
+
+// func
+
+const getSum1 = function(x, y) {
+    return x + y;
+}
+
+const getSum2 = new Function('x', 'y', 'return 1 + 43');
+console.log(getSum1(1,43));
+console.log(getSum2);
+
+// obj
+
+const josh1 = { name: 'Josh' };
+const josh2 = new Object({ name: 'Josh' });
+console.log(josh2);
+
+// arrays
+
+const arr1 = [1,2,3,4];
+const arr2 = new Array(1,2,3,4);
+console.log(arr2);
+
+// regular expressions
+
+const reg1 = /\w+/;
+const reg2 = new RegExp('\\w+');
+console.log(reg1);
+console.log(reg2);
