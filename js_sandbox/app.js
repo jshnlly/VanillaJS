e1960694d258eceeca87d5b1cb3cad911e6cabe8
Josh Nelson <jshnelson4@gmail.com>
2021-02-03 22:17:40 -0500

    arrow functions

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,45 +1,26 @@
-const posts = [
-    {title: 'Post one', body: 'this is post one'},
-    {title: 'Post one', body: 'this is post one'}
-];
-
-// function createPost() {
-//     setTimeout(function() {
-//         posts.push(post);
-//     }, 2000);
-// }
-
-// function getPosts() {
-//     setTimeout(function() {
-//         let output = '';
-//         posts.forEach(function(post) {
-//             output += `<li>${post.title}</li>`;
-//         }, 1000);
-
-//         document.body.innerHTML = output;
-//     });
-// }
-
-// createPost({title: 'post three', body: 'this is post three'});
-
-// getPosts();
-
-function createPost(post, callback) {
-    setTimeout(function() {
-        posts.push(post);
-        callback();
-    }, 2000);
+const sayHello = function() {
+    console.log('Hello');
 }
 
-function getPosts() {
-    setTimeout(function() {
-        let output = '';
-        posts.forEach(function(post) {
-            output += `<li>${post.title}</li>`;
-        }, 1000);
-
-        document.body.innerHTML = output;
-    });
+const sayHello = () => {
+    console.log('Hello');
 }
 
-createPost({title: 'post three', body: 'this is post three'}, getPosts);
+
+// one line function does not need braces
+const sayHello = () => console.log('helloooooo');
+
+const sayHello = () => 'Hello';
+
+const sayHello = () => ({ msg: 'Hello' });
+
+// single parameter does not need parenthesis
+const sayHello = name => console.log(`Hello ${name}`);
+
+const users = ['Nathan', 'John', 'William'];
+
+const nameLengths = users.map((name) => {
+    return name.length;
+})
+sayHello('Josh');
+
--- js_sandbox/fetch-api/app.js
+++ js_sandbox/fetch-api/app.js
@@ -6,49 +6,38 @@ document.getElementById('button3').addEventListener('click', getExternal);
 
 function getText() {
     fetch('test.txt')
-        .then(function(res) {
-            return res.text();
-        })
-        .then(function(data) {
+        .then(res => res.text())
+
+        .then(data => {
             console.log(data);
             document.getElementById('output').innerHTML = data;
         })
-        .catch(function(err) {
-            console.log(err);
-        });
+        .catch(err => console.log(err));
     }
 
     function getJSON() {
         fetch('posts.json')
-            .then(function(res) {
-                return res.json();
-            })
-            .then(function(data) {
+            .then(res => res.json())
+            .then(data => {
                 let output = '';
                 data.forEach(function(post) {
                     output += `<li>${post.title}</li>`;
                 });
                 document.getElementById('output').innerHTML = output;
             })
-            .catch(function(err) {
-                console.log(err);
-            });
+            .catch(err => console.log(err));
         }
 
 
         function getExternal() {
             fetch('https://api.github.com/users')
-                .then(function(res) {
-                    return res.json();
-                })
-                .then(function(data) {
+                .then(res => res.json())
+                .then(data => {
                     let output = '';
                     data.forEach(function(user) {
                         output += `<li>${user.login}</li>`;
                     });
                     document.getElementById('output').innerHTML = output;
                 })
-                .catch(function(err) {
-                    console.log(err);
-                });
+                .catch(err => console.log(err));
             }
